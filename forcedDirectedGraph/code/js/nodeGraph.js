22a91c30c68c3dca5261e930e3af9dd89e97fae0
Moises404 <sesmoi.moises@live.com>
2013-07-15 16:51:51 -0400

    almost working collision detection

--- forcedDirectedGraph/code/js/nodeGraph.js
+++ forcedDirectedGraph/code/js/nodeGraph.js
@@ -16,145 +16,106 @@ $(document).ready(function() {
 		console.log("UI is not loaded");
 	}
 
-	// test
-	var node1 = $(".node1");
+	// node test
+	var node1 = $(".node1-boundary");
 	var node1_x = node1.css("top");
 	var node1_y = node1.css("left");
 
-	var node2 = $(".node2");
+		// node1 boundary
+			var node1_top = node1.css("top");
+			var node1_left = node1.css("left");
+			var node1_right = parseInt(node1_left) + 50 + "px";
+			var node1_bottom = parseInt(node1_top) + 50 + "px";
+
+			var node1_boundary_top = parseInt(node1.css("top")) - 50 + "px";
+			var node1_boundary_left = parseInt(node1.css("left")) - 50 + "px";
+			var node1_boundary_right = parseInt(node1_left) + 100 + "px";
+			var node1_boundary_bottom = parseInt(node1_top) + 100 + "px";
+
+			// console.log("node1: " + "top: " + node1_top + ", left: " + node1_left + ", bottom: " + node1_bottom + ", right: " + node1_right);
+			// console.log("node1_boundary: " + "top: " + node1_boundary_top + ", left: " + node1_boundary_left + ", bottom: " + node1_boundary_bottom + ", right: " + node1_boundary_right);
+
+
+	var node2 = $(".node2-boundary");
 	var node2_x = node2.css("top");
 	var node2_y = node2.css("left");
 
+		// node2 boundary
+			var node2_top = node2.css("top");
+			var node2_left = node2.css("left");
+			var node2_right = parseInt(node2_left) + 50 + "px";
+			var node2_bottom = parseInt(node2_top) + 50 + "px";
+
+			var node2_boundary_top = parseInt(node2.css("top")) - 50 + "px";
+			var node2_boundary_left = parseInt(node2.css("left")) - 50 + "px";
+			var node2_boundary_right = parseInt(node2_left) + 100 + "px";
+			var node2_boundary_bottom = parseInt(node2_top) + 100 + "px";
+
+			// console.log("node2: " + "top: " + node2_top + ", left: " + node2_left + ", bottom: " + node2_bottom + ", right: " + node2_right);
+			// console.log("node2_boundary: " + "top: " + node2_boundary_top + ", left: " + node2_boundary_left + ", bottom: " + node2_boundary_bottom + ", right: " + node2_boundary_right);
 
 	var line1 = $(".line1");
 	line1.attr({"x1": node1_x, "y1": node1_y, "x2": node2_x, "y2": node2_y});
-
 
 		//draggable
 		node1.draggable({
 			drag: function () {
 				line1.attr({"x1": node1.css("left"), "y1": node1.css("top")});
+
+				// node1 boundary
+				node1_boundary_top = parseInt(node1.css("top")) - 50 + "px";
+				node1_boundary_left = parseInt(node1.css("left")) - 50 + "px";
+				node1_boundary_right = parseInt(node1_boundary_left) + 100 + "px";
+				node1_boundary_bottom = parseInt(node1_boundary_top) + 100 + "px";
+
+				// node2 boundary
+				node2_boundary_top = parseInt(node2.css("top")) - 50 + "px";
+				node2_boundary_left = parseInt(node2.css("left")) - 50 + "px";
+				node2_boundary_right = parseInt(node2_boundary_left) + 100 + "px";
+				node2_boundary_bottom = parseInt(node2_boundary_top) + 100 + "px";
+
+
+				// console.log("top: " + node1_top + ", left: " + node1_left + ", bottom: " + node1_bottom + ", right: " + node1_right);
+				// console.log("node2: " + "top: " + node2_boundary_top + ", left: " + node2_boundary_left + ", bottom: " + node2_boundary_bottom + ", right: " + node2_boundary_right);
+				// console.log("node1: " + "top: " + node1_boundary_top + ", left: " + node1_boundary_left + ", bottom: " + node1_boundary_bottom + ", right: " + node1_boundary_right);
+
+				collisionCallback();
 			}
 		});
 
 		node2.draggable({
 			drag: function () {
 				line1.attr({"x2": node2.css("left"), "y2": node2.css("top")});
+
+				// node1 boundary
+				node1_boundary_top = parseInt(node1.css("top")) - 50 + "px";
+				node1_boundary_left = parseInt(node1.css("left")) - 50 + "px";
+				node1_boundary_right = parseInt(node1_boundary_left) + 100 + "px";
+				node1_boundary_bottom = parseInt(node1_boundary_top) + 100 + "px";
+
+
+				// node2 boundary
+				node2_boundary_top = parseInt(node2.css("top")) - 50 + "px";
+				node2_boundary_left = parseInt(node2.css("left")) - 50 + "px";
+				node2_boundary_right = parseInt(node2_boundary_left) + 100 + "px";
+				node2_boundary_bottom = parseInt(node2_boundary_top) + 100 + "px";
+
+				// console.log("top: " + node2_top + ", left: " + node2_left + ", bottom: " + node2_bottom + ", right: " + node2_right);
+				// console.log("node2: " + "top: " + node2_boundary_top + ", left: " + node2_boundary_left + ", bottom: " + node2_boundary_bottom + ", right: " + node2_boundary_right);
+				// console.log("node1: " + "top: " + node1_boundary_top + ", left: " + node1_boundary_left + ", bottom: " + node1_boundary_bottom + ", right: " + node1_boundary_right);
+
+				collisionCallback();
+
 			}
 		});
 
-		
-
-	/////////////////////
-	/// NODE REPELLER ///
-	/////////////////////
-
-
-	Math.Vector = function (x,y) {
-	    this.x = x;
-	    this.y = y;
+	function collisionCallback() {
+		// console.log("callback");
+		if (node2_boundary_top < node1_boundary_bottom && 
+			node2_boundary_bottom > node1_boundary_top &&
+			node2_boundary_left < node1_boundary_right &&
+			node2_boundary_right > node1_boundary_left) {
+			console.log("collision!");
+		}
 	}
-	Math.Vector.prototype = {
-	    clone: function () {
-	        return new Math.Vector(this.x, this.y);
-	    },
-	    negate: function () {
-	        this.x = -this.x;
-	        this.y = -this.y;
-	        return this;
-	    },
-	    neg: function () {
-	        return this.clone().negate();
-	    },
-	    addeq: function (v) {
-	        this.x += v.x;
-	        this.y += v.y;
-	        return this;
-	    },
-	    subeq: function (v) {
-	        return this.addeq(v.neg());
-	    },
-	    add: function (v) {
-	        return this.clone().addeq(v);
-	    },
-	    sub: function (v) {
-	        return this.clone().subeq(v);
-	    },
-	    multeq: function (c) {
-	        this.x *= c;
-	        this.y *= c;
-	        return this;
-	    },
-	    diveq: function (c) {
-	        this.x /= c;
-	        this.y /= c;
-	        return this;
-	    },
-	    mult: function (c) {
-	        return this.clone().multeq(c);
-	    },
-	    div: function (c) {
-	        return this.clone().diveq(c);
-	    },
-
-	    dot: function (v) {
-	        return this.x * v.x + this.y * v.y;
-	    },
-	    length: function () {
-	        return Math.sqrt(this.dot(this));
-	    },
-	    normal: function () {
-	        return this.clone().diveq(this.length());
-	    }
-	};
-
-	function evade(evt) {
-	    var $this = $(this),
-	        corner = $this.offset(),
-	        center = {x: corner.left + $this.outerWidth() / 2, y: corner.top + $this.outerHeight() / 2},
-	        dist = new Math.Vector(center.x - evt.pageX, center.y - evt.pageY),
-	        closest = $this.outerWidth() / 2;
-	    
-	    // proximity test
-	    if (dist.length() >= closest) {
-	        return;
-	    }
-
-	    // calculate new position
-	    var delta = dist.normal().multeq(closest).sub(dist),
-	        newCorner = {left: corner.left + delta.x, top: corner.top + delta.y};
-
-	    // bounds check
-	    var padding = parseInt($this.css('padding-left'));
-	    if (newCorner.left < -padding) {
-	        newCorner.left = -padding;
-	    } else if (newCorner.left + $this.outerWidth() - padding > $(document).width()) {
-	        newCorner.left = $(document).width() - $this.outerWidth() + padding;
-	    }
-	    if (newCorner.top < -padding) {
-	        newCorner.top = -padding;
-	    } else if (newCorner.top + $this.outerHeight() - padding > $(document).height()) {
-	        newCorner.top = $(document).height() - $this.outerHeight() + padding;
-	    }
-
-	    // move bumper
-	    $this.offset(newCorner);
-	}
-
-	function beginEvade() {
-	    $(this).bind('mousemove', evade);
-	}
-
-	function endEvade() {
-	   $(this).unbind('mousemove', evade);
-	}
-
-
-	$(function () {
-	    $('.circle').wrap('<span class="bumper" />')
-
-	    $('.bumper').bind('mouseover', beginEvade);
-	    $('.bumper').bind('mouseout', endEvade);
-	});
-
 });
